00a46678b72a8d5814ff929d251e87e3be30f0e5
Wubbo Bos <wubbo@wubbobos.nl>
2016-12-07 22:41:10 +0100

    Finish Select class

--- src/Storage/Query/Select.php
+++ src/Storage/Query/Select.php
@@ -22,7 +22,14 @@ class Select
      *
      * @var string
      */
-    protected $table;
+    protected $tableName;
+
+    /**
+     * The joins
+     *
+     * @var array
+     */
+    protected $joins = [];
 
     /**
      * The columns
@@ -39,6 +46,41 @@ class Select
     protected $whereClause = '';
 
     /**
+     * The 'group by' clause
+     *
+     * @var string
+     */
+    protected $groupBy = '';
+
+    /**
+     * The 'order by' clause
+     *
+     * @var string
+     */
+    protected $orderBy = '';
+
+    /**
+     * The limit's offset
+     *
+     * @var int
+     */
+    protected $offsetBy = 0;
+
+    /**
+     * The limit
+     *
+     * @var int
+     */
+    protected $limitBy = 0;
+
+    /**
+     * The 'having' clause
+     *
+     * @var string
+     */
+    protected $havingClause = '';
+
+    /**
      * The current bind paramter index. Will be increased by one each time a named bind paramter is added
      *
      * @var int
@@ -53,7 +95,7 @@ class Select
     protected $bind = [];
 
     /**
-     * Sets the table for this query
+     * Generates an qualified (aliased) table name and adds the columns to the query if needed
      *
      * @param string|array $table
      *   Either a table name or an associative array with one element. In case of an array, the element's key should be the alias and the value the real table name.
@@ -63,15 +105,15 @@ class Select
      * @return Framewub\Storage\Query\Select
      *   Provides method chaining
      */
-    public function from($table, $columns = '*')
+    protected function tableStr($table, $columns = '*')
     {
         if (is_array($table)) {
             foreach ($table as $alias => $name) {
-                $this->table = "`{$name}` AS `{$alias}`";
+                $qualifiedName = "`{$name}` AS `{$alias}`";
                 break;
             }
         } else {
-            $this->table = "`{$table}`";
+            $qualifiedName = "`{$table}`";
             $alias = $table;
         }
 
@@ -91,36 +133,249 @@ class Select
             }
         }
 
+        return $qualifiedName;
+    }
+
+    /**
+     * Sets the table for this query
+     *
+     * @param string|array $table
+     *   Either a table name or an associative array with one element. In case of an array, the element's key should be the alias and the value the real table name.
+     * @param string|array $columns
+     *   OPTIONAL. The column(s) to select from the table. Defaults to '*'.
+     *
+     * @return Framewub\Storage\Query\Select
+     *   Provides method chaining
+     */
+    public function from($table, $columns = '*')
+    {
+        $this->tableName = $this->tableStr($table, $columns);
+
         return $this;
     }
 
+    /**
+     * Adds a join to this query
+     *
+     * @param string|array $table
+     *   Either a table name or an associative array with one element. In case of an array, the element's key should be the alias and the value the real table name.
+     * @param string $condition
+     *   The condition on which to join on
+     * @param string|array $columns
+     *   OPTIONAL. The column(s) to select from the table. Defaults to '*'.
+     *
+     * @return Framewub\Storage\Query\Select
+     *   Provides method chaining
+     */
+    public function join($table, $condition, $columns = '*')
+    {
+        $condition = "`" . preg_replace('/\s*=\s*/', '` = `', str_replace('.', '`.`', $condition)) . "`";
+        $join = "JOIN " . $this->tableStr($table, $columns) . " ON {$condition}";
+        $this->joins[] = $join;
+
+        return $this;
+    }
+
+    /**
+     * Adds a left join to this query
+     *
+     * @param string|array $table
+     *   Either a table name or an associative array with one element. In case of an array, the element's key should be the alias and the value the real table name.
+     * @param string $condition
+     *   The condition on which to join on
+     * @param string|array $columns
+     *   OPTIONAL. The column(s) to select from the table. Defaults to '*'.
+     *
+     * @return Framewub\Storage\Query\Select
+     *   Provides method chaining
+     */
+    public function joinLeft($table, $condition, $columns = '*')
+    {
+        $condition = "`" . preg_replace('/\s*=\s*/', '` = `', str_replace('.', '`.`', $condition)) . "`";
+        $join = "LEFT JOIN " . $this->tableStr($table, $columns) . " ON {$condition}";
+        $this->joins[] = $join;
+
+        return $this;
+    }
+
+    /**
+     * Adds a right join to this query
+     *
+     * @param string|array $table
+     *   Either a table name or an associative array with one element. In case of an array, the element's key should be the alias and the value the real table name.
+     * @param string $condition
+     *   The condition on which to join on
+     * @param string|array $columns
+     *   OPTIONAL. The column(s) to select from the table. Defaults to '*'.
+     *
+     * @return Framewub\Storage\Query\Select
+     *   Provides method chaining
+     */
+    public function joinRight($table, $condition, $columns = '*')
+    {
+        $condition = "`" . preg_replace('/\s*=\s*/', '` = `', str_replace('.', '`.`', $condition)) . "`";
+        $join = "RIGHT JOIN " . $this->tableStr($table, $columns) . " ON {$condition}";
+        $this->joins[] = $join;
+
+        return $this;
+    }
+
+    /**
+     * Adds a 'group by' clause to the query
+     *
+     * @param string|array $group
+     *   One or multiple columns to group by
+     *
+     * @return Framewub\Storage\Query\Select
+     *   Provides method chaining
+     */
+    public function group($group) {
+        $this->groupBy = is_array($group) ? "`" . implode("`, `", $group) . "`" : "`{$group}`";
+
+        return $this;
+    }
+
+    /**
+     * Adds a 'order by' clause to the query
+     *
+     * @param string|array $order
+     *   One or multiple columns to order by
+     *
+     * @return Framewub\Storage\Query\Select
+     *   Provides method chaining
+     */
+    public function order($order) {
+        $this->orderBy = is_array($order) ? "`" . implode("`, `", $order) . "`" : "`{$order}`";
+
+        return $this;
+    }
+
+    /**
+     * Adds an offset to the query
+     *
+     * @param int $offset
+     *   The offset
+     *
+     * @return Framewub\Storage\Query\Select
+     *   Provides method chaining
+     */
+    public function offset($offset) {
+        $this->offsetBy = (int)$offset;
+
+        return $this;
+    }
+
+    /**
+     * Adds a limit to the query
+     *
+     * @param int $limit
+     *   The limit
+     *
+     * @return Framewub\Storage\Query\Select
+     *   Provides method chaining
+     */
+    public function limit($limit) {
+        $this->limitBy = (int)$limit;
+
+        return $this;
+    }
+
+    /**
+     * Generates a bind parameter name and binds the specified value to it
+     *
+     * @param mixed $value
+     *   The value to bind
+     *
+     * @return string
+     *   The generated parameter name (beginning with a colon)
+     */
+    protected function bindValue($value) {
+        if ($value instanceof Func) {
+            return (string)$value;
+        }
+
+        $this->bindIndex++;
+        $key = ":bind{$this->bindIndex}";
+        $this->bind[$key] = $value;
+
+        return $key;
+    }
+
+    /**
+     * Binds a value to a specified parameter. Overwrites a previously bound value to that parameter if any
+     *
+     * @param string $param
+     *   The parameter name
+     * @param mixed $value
+     *   The value to bind
+     */
+    protected function bindParam($param, $value) {
+        $this->bind[$param] = $value;
+    }
+
+    /**
+     * Converts conditions into a part of a 'where' clause
+     *
+     * @param array $conditions
+     *   The conditions
+     * @param string $glue
+     *   OPTIONAL. Either 'AND' or 'OR'. Defaults to 'OR'
+     *
+     * @return string
+     *   The part of the 'where' clause
+     */
     protected function whereStr(&$conditions, $glue = 'AND')
     {
         $str = '';
         foreach ($conditions as $key => $value) {
-            if ($str) {
-                $str .= " {$glue} ";
-            }
-
-            $this->bindIndex++;
-            $operator = '=';
-            $operand = ':bind' . $this->bindIndex;
-
-            if (is_array($value) && !array_key_exists(0, $value)) {
-                $k = array_keys($value)[0];
-                $bindValue = $value[$k];
-
-                switch ($k) {
-                    case '$gt':
-                        $operator = '>';
-                        break;
+            if ($key == '$or' || $key == '$and') {
+                if ($str) {
+                    $str .= $key == '$or' ? ' OR ' : ' AND ';
                 }
+                $str .= "(" . $this->whereStr($value) . ")";
             } else {
-                $bindValue = $value;
-            }
+                if ($str) {
+                    $str .= " {$glue} ";
+                }
 
-            $str .= '`' . str_replace('.', '`.`', $key) . "` {$operator} {$operand}";
-            $this->bind[':bind'.$this->bindIndex] = $bindValue;
+                $operator = $value === null ? 'IS' : '=';
+                $operand = null;
+
+                if (is_array($value) && !array_key_exists(0, $value)) {
+                    $k = array_keys($value)[0];
+                    $bindValue = $value[$k];
+
+                    switch ($k) {
+                        case '$gt':
+                            $operator = '>';
+                            break;
+                        case '$gte':
+                            $operator = '>=';
+                            break;
+                        case '$lt':
+                            $operator = '<';
+                            break;
+                        case '$lte':
+                            $operator = '<=';
+                            break;
+                        case '$ne':
+                            $operator = $bindValue === null ? 'IS NOT' : '<>';
+                            break;
+                        case '$between':
+                            $operator = 'BETWEEN';
+                            $operand = $this->bindValue($bindValue[0]) . ' AND ' . $this->bindValue($bindValue[1]);
+                            break;
+                    }
+                } else {
+                    $bindValue = $value;
+                }
+
+                if (!$operand) {
+                    $operand = $bindValue === null ? 'NULL' : $this->bindValue($bindValue);
+                }
+
+                $str .= "(`" . str_replace('.', '`.`', $key) . "` {$operator} {$operand})";
+            }
         }
 
         return $str;
@@ -141,6 +396,63 @@ class Select
             $this->whereClause .= " AND ";
         }
         $this->whereClause .= $this->whereStr($conditions);
+
+        return $this;
+    }
+
+    /**
+     * Adds one or more conditions to the 'where' clause, appended with 'OR'
+     *
+     * @param array $conditions
+     *   The conditions to add
+     *
+     * @return Framewub\Storage\Query\Select
+     *   Provides method chaining
+     */
+    public function orWhere($conditions)
+    {
+        if ($this->whereClause) {
+            $this->whereClause .= " OR ";
+        }
+        $this->whereClause .= $this->whereStr($conditions, 'OR');
+
+        return $this;
+    }
+
+    /**
+     * Adds one or more conditions to the 'having' clause
+     *
+     * @param array $conditions
+     *   The conditions to add
+     *
+     * @return Framewub\Storage\Query\Select
+     *   Provides method chaining
+     */
+    public function having($conditions)
+    {
+        if ($this->havingClause) {
+            $this->havingClause .= " AND ";
+        }
+        $this->havingClause .= $this->whereStr($conditions);
+
+        return $this;
+    }
+
+    /**
+     * Adds one or more conditions to the 'having' clause, appended with 'OR'
+     *
+     * @param array $conditions
+     *   The conditions to add
+     *
+     * @return Framewub\Storage\Query\Select
+     *   Provides method chaining
+     */
+    public function orHaving($conditions)
+    {
+        if ($this->havingClause) {
+            $this->havingClause .= " OR ";
+        }
+        $this->havingClause .= $this->whereStr($conditions, 'OR');
 
         return $this;
     }
@@ -171,9 +483,28 @@ class Select
             }
         }
 
-        $sql = "SELECT " . implode(', ', $columns) . " FROM {$this->table}";
+        $sql = "SELECT " . implode(', ', $columns) . " FROM {$this->tableName}";
+        if (count($this->joins)) {
+            $sql .= " " . implode(" ", $this->joins);
+        }
         if ($this->whereClause) {
             $sql .= " WHERE {$this->whereClause}";
+        }
+        if ($this->groupBy) {
+            $sql .= " GROUP BY {$this->groupBy}";
+        }
+        if ($this->havingClause) {
+            $sql .= " HAVING {$this->havingClause}";
+        }
+        if ($this->orderBy) {
+            $sql .= " ORDER BY {$this->orderBy}";
+        }
+        if ($this->offsetBy || $this->limitBy) {
+            if ($this->offsetBy) {
+                $sql .= " LIMIT {$this->offsetBy}, " . ($this->limitBy ? $this->limitBy : "18446744073709551615");
+            } else {
+                $sql .= " LIMIT {$this->limitBy}";
+            }
         }
 
         return $sql;
--- test/Storage/Query/SelectTest.php
+++ test/Storage/Query/SelectTest.php
@@ -3,6 +3,7 @@
 use PHPUnit\Framework\TestCase;
 
 use Framewub\Storage\Query\Select;
+use Framewub\Storage\Query\Func;
 
 class SelectTest extends TestCase
 {
@@ -18,6 +19,134 @@ class SelectTest extends TestCase
         $this->assertEquals($select, $result, "Method 'from' should return the Select object for chaining");
         // Assert the resulting query
         $this->assertEquals("SELECT `foo`.* FROM `foo`", (string)$select);
+
+        // Test alias
+        $select = new Select();
+        // Add 'from' clause
+        $select->from([ 'f' => 'foo' ]);
+        // Assert the resulting query
+        $this->assertEquals("SELECT `f`.* FROM `foo` AS `f`", (string)$select);
+    }
+
+    public function testJoin()
+    {
+        // Create object
+        $select = new Select();
+
+        // Add 'from' clause
+        $result = $select->from('foo')
+            ->join('bar', 'bar.id = foo.bar_id');
+
+        // Assert chaining
+        $this->assertEquals($select, $result, "Method 'from' should return the Select object for chaining");
+        // Assert the resulting query
+        $this->assertEquals("SELECT `foo`.*, `bar`.* FROM `foo` JOIN `bar` ON `bar`.`id` = `foo`.`bar_id`", (string)$select);
+
+        // Test join with alias
+        $select = new Select();
+        $result = $select->from('foo')
+            ->join([ 'b' => 'bar' ], 'b.id = foo.bar_id');
+        // Assert the resulting query
+        $this->assertEquals("SELECT `foo`.*, `b`.* FROM `foo` JOIN `bar` AS `b` ON `b`.`id` = `foo`.`bar_id`", (string)$select);
+
+        // Test left join
+        $select = new Select();
+        $result = $select->from('foo')
+            ->joinLeft('bar', 'bar.id = foo.bar_id');
+        // Assert the resulting query
+        $this->assertEquals("SELECT `foo`.*, `bar`.* FROM `foo` LEFT JOIN `bar` ON `bar`.`id` = `foo`.`bar_id`", (string)$select);
+
+        // Test left join with alias
+        $select = new Select();
+        $result = $select->from('foo')
+            ->joinLeft([ 'b' => 'bar' ], 'b.id = foo.bar_id');
+        // Assert the resulting query
+        $this->assertEquals("SELECT `foo`.*, `b`.* FROM `foo` LEFT JOIN `bar` AS `b` ON `b`.`id` = `foo`.`bar_id`", (string)$select);
+
+        // Test right join
+        $select = new Select();
+        $result = $select->from('foo')
+            ->joinRight('bar', 'bar.id = foo.bar_id');
+        // Assert the resulting query
+        $this->assertEquals("SELECT `foo`.*, `bar`.* FROM `foo` RIGHT JOIN `bar` ON `bar`.`id` = `foo`.`bar_id`", (string)$select);
+
+        // Test right join with alias
+        $select = new Select();
+        $result = $select->from('foo')
+            ->joinRight([ 'b' => 'bar' ], 'b.id = foo.bar_id');
+        // Assert the resulting query
+        $this->assertEquals("SELECT `foo`.*, `b`.* FROM `foo` RIGHT JOIN `bar` AS `b` ON `b`.`id` = `foo`.`bar_id`", (string)$select);
+    }
+
+    public function testGroup()
+    {
+        // Create object
+        $select = new Select();
+
+        // Add 'from' clause
+        $result = $select->from('foo')
+            ->group('bar');
+        // Assert chaining
+        $this->assertEquals($select, $result, "Method 'group' should return the Select object for chaining");
+        // Assert the resulting query
+        $this->assertEquals("SELECT `foo`.* FROM `foo` GROUP BY `bar`", (string)$select);
+
+        // Test multiple groups
+        $select = new Select();
+        $select->from('foo')
+            ->group([ 'bar', 'lorem' ]);
+        $this->assertEquals("SELECT `foo`.* FROM `foo` GROUP BY `bar`, `lorem`", (string)$select);
+    }
+
+    public function testOrder()
+    {
+        // Create object
+        $select = new Select();
+
+        // Add 'from' clause
+        $result = $select->from('foo')
+            ->order('bar');
+        // Assert chaining
+        $this->assertEquals($select, $result, "Method 'order' should return the Select object for chaining");
+        // Assert the resulting query
+        $this->assertEquals("SELECT `foo`.* FROM `foo` ORDER BY `bar`", (string)$select);
+
+        // Test multiple groups
+        $select = new Select();
+        $select->from('foo')
+            ->order([ 'bar', 'lorem' ]);
+        $this->assertEquals("SELECT `foo`.* FROM `foo` ORDER BY `bar`, `lorem`", (string)$select);
+    }
+
+    public function testOffsetAndLimit()
+    {
+        // Create object
+        $select = new Select();
+        // Add 'offset' clause
+        $result = $select->from('foo')
+            ->offset(10);
+        // Assert chaining
+        $this->assertEquals($select, $result, "Method 'offset' should return the Select object for chaining");
+        // Assert the resulting query
+        $this->assertEquals("SELECT `foo`.* FROM `foo` LIMIT 10, 18446744073709551615", (string)$select);
+
+        // Create object
+        $select = new Select();
+        // Add 'limit' clause
+        $result = $select->from('foo')
+            ->limit(10);
+        // Assert chaining
+        $this->assertEquals($select, $result, "Method 'limit' should return the Select object for chaining");
+        // Assert the resulting query
+        $this->assertEquals("SELECT `foo`.* FROM `foo` LIMIT 10", (string)$select);
+
+        // Create object
+        $select = new Select();
+        // Add 'limit' clause
+        $select->from('foo')
+            ->limit(42)->offset(10);
+        // Assert the resulting query
+        $this->assertEquals("SELECT `foo`.* FROM `foo` LIMIT 10, 42", (string)$select);
     }
 
     public function testFromWithColumns()
@@ -52,18 +181,176 @@ class SelectTest extends TestCase
         // Test simple comparison
         $result = $select->where([ 'id' => 1 ]);
         // Assert chaining
-        $this->assertEquals($select, $result, "Method 'from' should return the Select object for chaining");
+        $this->assertEquals($select, $result, "Method 'where' should return the Select object for chaining");
         // Assert the resulting query
-        $this->assertEquals("SELECT `foo`.* FROM `foo` WHERE `id` = :bind1", (string)$select);
+        $this->assertEquals("SELECT `foo`.* FROM `foo` WHERE (`id` = :bind1)", (string)$select);
         // Assert bind params
         $bind = $select->getBind();
         $this->assertArrayHasKey(':bind1', $bind);
         $this->assertEquals($bind[':bind1'], 1);
 
+        // Test 'OR'
+        $select = new Select();
+        $select->from('foo')
+            ->where([ 'id' => 1 ])
+            ->orWhere([ 'id' => 3, 'foo' => 'bar' ]);
+        $this->assertEquals("SELECT `foo`.* FROM `foo` WHERE (`id` = :bind1) OR (`id` = :bind2) OR (`foo` = :bind3)", (string)$select);
+
         // Test greater than
         $select = new Select();
         $select->from('foo')
             ->where([ 'id' => [ '$gt' => 3 ] ]);
-        $this->assertEquals("SELECT `foo`.* FROM `foo` WHERE `id` > :bind1", (string)$select);
+        $this->assertEquals("SELECT `foo`.* FROM `foo` WHERE (`id` > :bind1)", (string)$select);
+
+        // Test greater than or equal
+        $select = new Select();
+        $select->from('foo')
+            ->where([ 'id' => [ '$gte' => 3 ] ]);
+        $this->assertEquals("SELECT `foo`.* FROM `foo` WHERE (`id` >= :bind1)", (string)$select);
+
+        // Test less than
+        $select = new Select();
+        $select->from('foo')
+            ->where([ 'id' => [ '$lt' => 3 ] ]);
+        $this->assertEquals("SELECT `foo`.* FROM `foo` WHERE (`id` < :bind1)", (string)$select);
+
+        // Test less than or equal
+        $select = new Select();
+        $select->from('foo')
+            ->where([ 'id' => [ '$lte' => 3 ] ]);
+        $this->assertEquals("SELECT `foo`.* FROM `foo` WHERE (`id` <= :bind1)", (string)$select);
+
+        // Test less not equal
+        $select = new Select();
+        $select->from('foo')
+            ->where([ 'id' => [ '$ne' => 3 ] ]);
+        $this->assertEquals("SELECT `foo`.* FROM `foo` WHERE (`id` <> :bind1)", (string)$select);
+
+        // Test IS NULL
+        $select = new Select();
+        $select->from('foo')
+            ->where([ 'id' => null ]);
+        $this->assertEquals("SELECT `foo`.* FROM `foo` WHERE (`id` IS NULL)", (string)$select);
+
+        // Test IS NOT NULL
+        $select = new Select();
+        $select->from('foo')
+            ->where([ 'id' => [ '$ne' => null ] ]);
+        $this->assertEquals("SELECT `foo`.* FROM `foo` WHERE (`id` IS NOT NULL)", (string)$select);
+
+        // Test literal expression
+        $select = new Select();
+        $select->from('foo')
+            ->where([ 'created' => new Func("NOW()") ]);
+        $this->assertEquals("SELECT `foo`.* FROM `foo` WHERE (`created` = NOW())", (string)$select);
+
+        // Test between
+        $select = new Select();
+        $select->from('foo')
+            ->where([ 'id' => [ '$between' => [ 3, 10 ] ] ]);
+        $this->assertEquals("SELECT `foo`.* FROM `foo` WHERE (`id` BETWEEN :bind1 AND :bind2)", (string)$select);
+        // Assert bind params
+        $bind = $select->getBind();
+        $this->assertArrayHasKey(':bind1', $bind);
+        $this->assertArrayHasKey(':bind2', $bind);
+        $this->assertEquals($bind[':bind1'], 3);
+        $this->assertEquals($bind[':bind2'], 10);
+    }
+
+    public function testNestedWhere()
+    {
+        // Create object
+        $select = new Select();
+
+        // Add 'from' clause
+        $select->from('foo');
+
+        // Test simple comparison
+        $result = $select->where([ 'id' => 1, '$or' => [ 'foo' => 'bar', 'dingen' => 'zaken' ] ]);
+        // Assert the resulting query
+        $this->assertEquals("SELECT `foo`.* FROM `foo` WHERE (`id` = :bind1) OR ((`foo` = :bind2) AND (`dingen` = :bind3))", (string)$select);
+        // Assert bind params
+        $bind = $select->getBind();
+        $this->assertArrayHasKey(':bind1', $bind);
+        $this->assertArrayHasKey(':bind2', $bind);
+        $this->assertArrayHasKey(':bind3', $bind);
+        $this->assertEquals($bind[':bind1'], 1);
+        $this->assertEquals($bind[':bind2'], 'bar');
+        $this->assertEquals($bind[':bind3'], 'zaken');
+    }
+
+    public function testHaving()
+    {
+        // Create object
+        $select = new Select();
+
+        // Add 'from' clause
+        $select->from('foo')->where([ 'lorem' => 'ipsum' ]);
+
+        // Test simple comparison
+        $result = $select->having([ 'id' => 1 ]);
+        // Assert chaining
+        $this->assertEquals($select, $result, "Method 'having' should return the Select object for chaining");
+        // Assert the resulting query
+        $this->assertEquals("SELECT `foo`.* FROM `foo` WHERE (`lorem` = :bind1) HAVING (`id` = :bind2)", (string)$select);
+        // Assert bind params
+        $bind = $select->getBind();
+        $this->assertArrayHasKey(':bind2', $bind);
+        $this->assertEquals($bind[':bind2'], 1);
+
+        // Test 'OR'
+        $select = new Select();
+        $select->from('foo')->where([ 'lorem' => 'ipsum' ])
+            ->having([ 'id' => 1 ])
+            ->orHaving([ 'id' => 3, 'foo' => 'bar' ]);
+        $this->assertEquals("SELECT `foo`.* FROM `foo` WHERE (`lorem` = :bind1) HAVING (`id` = :bind2) OR (`id` = :bind3) OR (`foo` = :bind4)", (string)$select);
+
+        // Test greater than
+        $select = new Select();
+        $select->from('foo')->where([ 'lorem' => 'ipsum' ])
+            ->having([ 'id' => [ '$gt' => 3 ] ]);
+        $this->assertEquals("SELECT `foo`.* FROM `foo` WHERE (`lorem` = :bind1) HAVING (`id` > :bind2)", (string)$select);
+
+        // Test greater than or equal
+        $select = new Select();
+        $select->from('foo')->where([ 'lorem' => 'ipsum' ])
+            ->having([ 'id' => [ '$gte' => 3 ] ]);
+        $this->assertEquals("SELECT `foo`.* FROM `foo` WHERE (`lorem` = :bind1) HAVING (`id` >= :bind2)", (string)$select);
+
+        // Test less than
+        $select = new Select();
+        $select->from('foo')->where([ 'lorem' => 'ipsum' ])
+            ->having([ 'id' => [ '$lt' => 3 ] ]);
+        $this->assertEquals("SELECT `foo`.* FROM `foo` WHERE (`lorem` = :bind1) HAVING (`id` < :bind2)", (string)$select);
+
+        // Test less than or equal
+        $select = new Select();
+        $select->from('foo')->where([ 'lorem' => 'ipsum' ])
+            ->having([ 'id' => [ '$lte' => 3 ] ]);
+        $this->assertEquals("SELECT `foo`.* FROM `foo` WHERE (`lorem` = :bind1) HAVING (`id` <= :bind2)", (string)$select);
+
+        // Test less not equal
+        $select = new Select();
+        $select->from('foo')->where([ 'lorem' => 'ipsum' ])
+            ->having([ 'id' => [ '$ne' => 3 ] ]);
+        $this->assertEquals("SELECT `foo`.* FROM `foo` WHERE (`lorem` = :bind1) HAVING (`id` <> :bind2)", (string)$select);
+
+        // Test IS NULL
+        $select = new Select();
+        $select->from('foo')->where([ 'lorem' => 'ipsum' ])
+            ->having([ 'id' => null ]);
+        $this->assertEquals("SELECT `foo`.* FROM `foo` WHERE (`lorem` = :bind1) HAVING (`id` IS NULL)", (string)$select);
+
+        // Test between
+        $select = new Select();
+        $select->from('foo')->where([ 'lorem' => 'ipsum' ])
+            ->having([ 'id' => [ '$between' => [ 3, 10 ] ] ]);
+        $this->assertEquals("SELECT `foo`.* FROM `foo` WHERE (`lorem` = :bind1) HAVING (`id` BETWEEN :bind2 AND :bind3)", (string)$select);
+        // Assert bind params
+        $bind = $select->getBind();
+        $this->assertArrayHasKey(':bind2', $bind);
+        $this->assertArrayHasKey(':bind3', $bind);
+        $this->assertEquals($bind[':bind2'], 3);
+        $this->assertEquals($bind[':bind3'], 10);
     }
 }
