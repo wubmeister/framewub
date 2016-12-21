b085411a859ce2eef80421641b2008340a18db53
Wubbo Bos <wubbo@wubbobos.nl>
2016-12-21 23:28:09 +0100

    Add Abstract REST Controller

---
+++ src/Rest/AbstractController.php
@@ -0,0 +1,118 @@
+<?php
+
+/**
+ * Base class for all RESTful request handlers
+ *
+ * @package    framewub/rest
+ * @author     Wubbo Bos <wubbo@wubbobos.nl>
+ * @copyright  Copyright (c) Wubbo Bos
+ * @license    GPL
+ * @link       https://github.com/wubmeister/framewub
+ */
+
+namespace Framewub\Rest;
+
+use Framewub\Http\Message\ServerRequest;
+use Framewub\Http\Message\Response\Json as JsonResponse;
+
+abstract class AbstractController
+{
+    /**
+     * Handles a request and gives a response
+     *
+     * @param Framewub\Http\Message\ServerRequest $request
+     *   The request
+     *
+     * @return Framewub\Http\Message\Response
+     *   The response
+     */
+    public function __invoke(ServerRequest $request)
+    {
+        $id = $request->getAttribute('id');
+        $result = [ 'success' => false ];
+
+        switch ($request->getMethod()) {
+            case 'GET':
+                if ($id) {
+                    $result = $this->findById($id);
+                } else {
+                    $result = $this->findAll();
+                }
+                break;
+
+            case 'PUT':
+            case 'POST':
+                if ($id) {
+                    $result = $this->update($id, $request->getParsedBody());
+                } else {
+                    $result = $this->add($request->getParsedBody());
+                }
+                break;
+
+            case 'DELETE':
+                if ($id) {
+                    $result = $this->delete($id);
+                }
+                break;
+        }
+
+        return new JsonResponse($result);
+    }
+
+    /**
+     * This method MUST return all the visible objects in the collection
+     *
+     * @return array
+     *   The objects
+     */
+    abstract protected function findAll();
+
+    /**
+     * This method MUST return an object identified by the given ID or null if
+     * there is no object with that ID
+     *
+     * @param mixed $id
+     *   The ID
+     *
+     * @return array|object|null
+     *   The object
+     */
+    abstract protected function findById($id);
+
+    /**
+     * This method MUST create an object with the given values, if the values
+     * are valid.
+     *
+     * @param array|object $values
+     *   The values
+     *
+     * @return array|object|null
+     *   The newly created object
+     */
+    abstract protected function add($values);
+
+    /**
+     * This method MUST update an existing object with the given values, if the
+     * values are valid.
+     *
+     * @param mixed $id
+     *   The object's ID
+     * @param array|object $values
+     *   The values
+     *
+     * @return array|object|null
+     *   The newly created object
+     */
+    abstract protected function update($id, $values);
+
+    /**
+     * This method MUST delete an existing object with the given ID.
+     *
+     * @param mixed $id
+     *   The object's ID
+     *
+     * @return array|object|null
+     *   Status information about the deletion
+     */
+    abstract protected function delete($id);
+}
---
+++ test/Rest/AbstractControllerTest.php
@@ -0,0 +1,147 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+use Framewub\Rest\AbstractController;
+use Framewub\Http\Message\ServerRequest;
+use Framewub\Http\Message\Response;
+
+class MockController extends AbstractController
+{
+    protected function findAll()
+    {
+        return [
+            [
+                'name' => 'Lorem ipsum',
+                'description' => 'Lorem ipsum, doler sit amet'
+            ],
+            [
+                'name' => 'Foo bar',
+                'description' => 'This is just dome dummy data'
+            ]
+        ];
+    }
+
+    protected function findById($id)
+    {
+        switch ($id) {
+            case 1:
+                return [
+                    'name' => 'Lorem ipsum',
+                    'description' => 'Lorem ipsum, doler sit amet'
+                ];
+                break;
+
+            case 2:
+                return [
+                    'name' => 'Foo bar',
+                    'description' => 'This is just dome dummy data'
+                ];
+                break;
+        }
+    }
+
+    protected function add($values)
+    {
+        return $values;
+    }
+
+    protected function update($id, $values)
+    {
+        return array_merge([ 'id' => $id ], $values);
+    }
+
+    protected function delete($id)
+    {
+        return [ 'success' => true, 'message' => 'Deleted #'.$id, 'id' => $id ];
+    }
+}
+
+class AbstractControllerTest extends TestCase
+{
+    public function testFindAll()
+    {
+        $controller = new MockController();
+        ob_start();
+        $response = $controller(new ServerRequest());
+        ob_end_clean();
+        $this->assertInstanceOf(Response::class, $response);
+        $this->assertEquals(
+            '[{"name":"Lorem ipsum","description":"Lorem ipsum, doler sit amet"},{"name":"Foo bar","description":"This is just dome dummy data"}]',
+            $response->getBody()->getMockContents()
+        );
+    }
+
+    public function testFindOne()
+    {
+        $controller = new MockController();
+        ob_start();
+        $response = $controller(new ServerRequest([ 'id' => 1 ]));
+        ob_end_clean();
+        $this->assertInstanceOf(Response::class, $response);
+        $this->assertEquals(
+            '{"name":"Lorem ipsum","description":"Lorem ipsum, doler sit amet"}',
+            $response->getBody()->getMockContents()
+        );
+
+        ob_start();
+        $response = $controller(new ServerRequest([ 'id' => 2 ]));
+        ob_end_clean();
+        $this->assertInstanceOf(Response::class, $response);
+        $this->assertEquals(
+            '{"name":"Foo bar","description":"This is just dome dummy data"}',
+            $response->getBody()->getMockContents()
+        );
+    }
+
+    public function testInsert()
+    {
+        $_POST['name'] = "Trens roxnas et plokeing";
+        $_POST['description'] = "Lucius in domus est";
+
+        $controller = new MockController();
+        $request = new ServerRequest();
+        ob_start();
+        $response = $controller($request->withMethod('POST'));
+        ob_end_clean();
+        $this->assertInstanceOf(Response::class, $response);
+        $this->assertEquals(
+            '{"name":"Trens roxnas et plokeing","description":"Lucius in domus est"}',
+            $response->getBody()->getMockContents()
+        );
+    }
+
+    public function testUpdate()
+    {
+        $_POST['name'] = "Updated stuff";
+        $_POST['description'] = "Lucius in domus est";
+
+        $controller = new MockController();
+        $request = new ServerRequest([ 'id' => 3 ]);
+        ob_start();
+        $response = $controller($request->withMethod('PUT'));
+        ob_end_clean();
+        $this->assertInstanceOf(Response::class, $response);
+        $this->assertEquals(
+            '{"id":3,"name":"Updated stuff","description":"Lucius in domus est"}',
+            $response->getBody()->getMockContents()
+        );
+    }
+
+    public function testDelete()
+    {
+        $_POST['name'] = "Updated stuff";
+        $_POST['description'] = "Lucius in domus est";
+
+        $controller = new MockController();
+        $request = new ServerRequest([ 'id' => 3 ]);
+        ob_start();
+        $response = $controller($request->withMethod('DELETE'));
+        ob_end_clean();
+        $this->assertInstanceOf(Response::class, $response);
+        $this->assertEquals(
+            '{"success":true,"message":"Deleted #3","id":3}',
+            $response->getBody()->getMockContents()
+        );
+    }
+}
