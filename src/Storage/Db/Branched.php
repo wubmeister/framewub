5b7aebc30dbc3937d55c28e46b065cc2c17c46f8
Wubbo Bos <wubbo@addnoise.nl>
2017-01-12 17:39:08 +0100

    Begin of Branched DB storage

---
+++ src/Storage/Db/Branched.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Class to represent a branched storage in the database
+ *
+ * @package    framewub/storage
+ * @author     Wubbo Bos <wubbo@wubbobos.nl>
+ * @copyright  Copyright (c) Wubbo Bos
+ * @license    GPL
+ * @link       https://github.com/wubmeister/framewub
+ */
+
+namespace Framewub\Storage\Db;
+
+class Branched extends AbstractStorage
+{
+}
---
+++ test/Storage/Db/BranchedTest.php
@@ -0,0 +1,13 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+use Framewub\Storage\Db\Branched;
+
+class BranchedTest extends TestCase
+{
+    public function testFetchTree()
+    {
+
+    }
+}
